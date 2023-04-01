3427ecc32ffdf03f8b866fd03b79b07ffc38265d
Rafael <rafollo@live.com>
2023-04-01 11:48:14 -0300

    Prettified next config js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,11 +2,9 @@ require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
-module.exports = {
   env: {
     NEXT_PUBLIC_CHAIN_ID: 1313161555
   }
 }
+
+module.exports = nextConfig
